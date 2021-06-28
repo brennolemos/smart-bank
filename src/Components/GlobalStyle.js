68dfa52e8a7cdfcbf9e69b1d36b781b6a0db24d6
Breno Lemos <brenomiros@gmail.com>
2021-06-28 19:30:26 -0300

    Global styles file

---
+++ src/Components/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap");
+
+  * {
+    box-sizing: border-box;
+    font-family: "Montserrat", sans-serif;
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+    color: grey;
+  }
+`
